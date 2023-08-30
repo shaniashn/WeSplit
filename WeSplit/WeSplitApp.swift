6b62bde550efd5e4112e14802a0ec592b628e6da
Sha Nia Siahaan <shaniashn76@gmail.com>
2023-08-31 00:52:02 +0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sha Nia Siahaan on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
